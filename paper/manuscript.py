22b9df5740b6b475d7e7f34a08e380380aa70bc9
microway <romanog@mit.edu>
2018-06-28 16:04:52 -0400

    updated paper

--- paper/manuscript.py
+++ paper/manuscript.py
@@ -1,6 +1,6 @@
 from pypapers.manuscript import Manuscript as M
 
-p = M(biblio_file ='biblio.bib',journal='prl',numberline=False)
+p = M(biblio_file ='biblio.bib',journal='prl',numberline=False,twocolumn=False)
 
 MIT = 'Department of Mechanical Engineering, Massachusetts Institute of Technology, 77 Massachusetts Avenue, Cambridge, MA 02139, USA'
 
@@ -34,9 +34,8 @@ p.set_title('Byesim: a tool for fast device characterization with Bayesian infer
 
 p.add_abstract_sentence(r'''abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract ''')
 
-con = p.get_new_section_id('Introduction')
+p.new_section('Introduction')
 
-p.s('introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction introduction ')
 #p.s('Example of referencens as seen in ' + p.cite('fugallo2014thermal') + r''' and this refers to fig ''' + p.ref(fig_1 + 'a') + r'''referes the Eq. ''' + p.ref(nn1),br=1)
 
 #p.add_equation(r'''E=mc^2 ''',label=nn1)
@@ -44,30 +43,32 @@ p.s('introduction introduction introduction introduction introduction introducti
 #p.add_figure(filename='figure_1',caption='',label=fig_1,center_page = True)
 
 
-con = p.get_new_section_id('Model')
-p.s('Figure 1: high-level flowchart')
+p.new_section('Model')
+#p.s('Figure 1: high-level flowchart')
 
-con = p.get_new_section_id('Software Architecture')
-p.s('Figure 2: detailed workflow (including iteration to update posterior)')
+p.new_section('Software Architecture')
+p.s('Figure 1: detailed workflow (including iteration to update posterior)')
 
-con = p.get_new_section_id('Example with diode - validation')
-p.s('Figure 3: diode fit stuff')
+p.add_figure(filename='figure_1',caption='(a) Scheme (b) Probability',center_page = False,label=fig_1)
+p.add_figure(filename='figure_2',caption='Bayesian workflow',center_page = False,label = fig_2)
 
-con = p.get_new_section_id('Example with real data')
+p.new_section('Example with diode - validation')
+p.s('Figure 2: diode fit stuff')
+p.add_figure(filename='figure_3',caption='Ideal diode',center_page = False,label = fig_3)
+
+p.new_section('Example with real data')
 p.s('Figure 4: fitting real data (maybe resistive diode?)')
+p.add_figure(filename='figure_4',caption='Real data',center_page = False,label=fig_4)
 
-con = p.get_new_section_id('Discussion')
-p.s('discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion discussion ')
+con = p.new_section('Discussion')
 
-con = p.get_new_section_id('Conclusions')
-p.s('conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions conclusions ')
-
-
-con = p.get_new_section_id('Acknowledgements')
-p.s(r'''acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements acknowledgements ''')
+p.new_section('Conclusions')
 
 
-con = p.get_new_section_id('Appendix')
+p.new_section('Acknowledgements')
+
+
+p.new_section('Appendix')
 p.s('include minimal code to run diode example')
 
 p.tex()
