bc3b2c927cd95033315ba078a5dbf0e22e268438
Giuseppe Romano <gromano@pop-os.localdomain>
2018-07-31 09:47:55 -0500

    added docs framework

--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Bayesim&#8217;s documentation!"],objects:{},titleterms:{bayesim:0,document:0,welcom:0,indic:0,tabl:0}})
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Bayesim\u2019s documentation!"],titleterms:{bayesim:0,document:0,indic:0,tabl:0,welcom:0}})
