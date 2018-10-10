fe0a8fe52e5fb5b1d91d35878dfdf699fc81cdda
rkurchin <rkurchin@mit.edu>
2018-10-10 10:06:20 -0400

    committing before repackaging

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 import os
 
-
 setup(name='bayesim',
-      version='0.9.4',
+      version='0.9.5',
       description='Fast model fitting via Bayesian inference',
       author='Rachel Kurchin, Giuseppe Romano',
       author_email='rkurchin@mit.edu, romanog@mit.edu',
-      classifiers=['Programming Language :: Python :: 2.7',\
-                   'Programming Language :: Python :: 3.6'],
+      classifiers=['Programming Language :: Python :: 3.6'],
       long_description=open('README.rst').read(),
       install_requires=['pandas',
                         'deepdish',
