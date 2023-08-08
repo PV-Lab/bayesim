883f951b254ae4bcb9ea72e257f0754b34f1ba04
Rachel Kurchin <rkurchin@cmu.edu>
2023-08-08 09:55:46 -0400

    update RCK's email, also tables not pytables, patch version bump
    Signed-off-by: Rachel Kurchin <rkurchin@cmu.edu>

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 import os, sys
 
 setup(name='bayesim',
-      version='0.10.0',
+      version='0.10.1',
       description='Fast model fitting via Bayesian inference',
       author='Rachel Kurchin, Giuseppe Romano',
-      author_email='rkurchin@mit.edu, romanog@mit.edu',
+      author_email='rkurchin@alum.mit.edu, romanog@mit.edu',
       classifiers=['Programming Language :: Python :: 3.7'],
       long_description=open('README.rst').read(),
-      install_requires=['pandas', 'scipy', 'matplotlib', 'joblib', 'pytables'],
+      install_requires=['pandas', 'scipy', 'matplotlib', 'joblib', 'tables'],
       license='GPLv2',
       packages = ['bayesim'],
       entry_points = {
